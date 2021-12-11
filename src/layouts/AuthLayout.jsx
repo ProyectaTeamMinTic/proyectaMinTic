bf2ebaf9170af0b16e9f2e5868e4e1035f96e6c6
FranklinZ12 <franklinq8@gmail.com>
2021-12-10 23:50:19 -0500

    flecha de devolver desde login a LandingPage agregada

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import { Link } from 'react-router-dom';
+
 
 const AuthLayout = () => {
     return (
         <div className='flex flex-col md:flex-row flex-no-wrap h-screen'>
             <div className='flex w-full h-full'>
                 <div className='w-full h-full  overflow-y-scroll'>
-                    Layout de Autenticacion
+                    <Link to='/'>
+                        <i className='fas fa-arrow-left text-gray-600 cursor-pointer font-bold text-xl hover:text-gray-900 pl-9 pt-5' />
+                    </Link>
                     <Outlet />
                 </div>
             </div>
