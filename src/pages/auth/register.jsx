5c24669356b75cdba214c600d237a56af21fbcd2
FranklinZ12 <franklinq8@gmail.com>
2021-12-13 19:35:41 -0500

    agregando ruta de logo en registro

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -9,6 +9,7 @@ import { REGISTRO } from "graphql/auth/mutations";
 import { useMutation } from "@apollo/client";
 import { useNavigate } from "react-router";
 import { useAuth } from "context/authContext";
+import logo from "media/logo_blue.png";
 
 const Register = () => {
   const { setToken } = useAuth();
